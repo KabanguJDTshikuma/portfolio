6bd0a098f73ded4441686f6e8acf8f9f028cb070
KabanguJDTshikuma <kabangufuture@gmail.com>
2019-06-11 16:52:15 +0200

    Add detail page and Polish

--- blog/models.py
+++ blog/models.py
@@ -2,9 +2,15 @@ from django.db import models
 
 class Blog(models.Model):
 	title = models.CharField(max_length=250)
-	image = models.ImageField(upload_to='images/')
-	body = models.TextField() 
 	pub_date = models.DateTimeField()
+	body = models.TextField() 
+	image = models.ImageField(upload_to='images/')
+
+	def summary(self):
+		return self.body[:100]
+
+	def pub_date_prettry(self):
+		return self.pub_date.strftime('%b %e %Y')
 
 	def __str__(self):
 		return self.title
