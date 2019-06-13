5eec38c4074d586b23de82588531f537e28aee3e
KabanguJDTshikuma <kabangufuture@gmail.com>
2019-06-13 16:40:19 +0200

    last

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,10 +80,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
-        'USER': 'postgres',
-        'PASSWORD': 'godbless',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
